3c71eb4f61b5c3ed279dc8cd4749f6f16e68db81
Vitaliy Kotyak <v.kotyak@gmail.com>
2020-09-13 11:41:31 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Confused)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    basic.pause(500)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.pause(500)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+    basic.pause(500)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    basic.pause(500)
+})
+basic.showIcon(IconNames.Happy)
+basic.pause(100)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+})
